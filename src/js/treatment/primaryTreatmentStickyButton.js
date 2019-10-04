4e13df63c611a4c0135f12bed9f4f04bef970a01
Mats Tostrup <matstostrup@gmail.com>
2019-10-04 13:53:37 +0200

    Scrollhandler er nå mer effektiv

--- src/js/treatment/primaryTreatmentStickyButton.js
+++ src/js/treatment/primaryTreatmentStickyButton.js
@@ -1,46 +1,42 @@
 function primaryTreatmentStickyButton() {
     var debouncedEventHandler = debounce(function() {
         var pt = $('.o_primary-treatment.expanded');
+        
         if (pt && pt.length !== 0) {
-            var stickyNav = pt.find('.js-sticky-expander-nav');
-            var toTopButton = stickyNav.find('.scroll-exp');
-            toTopButton.off('click.primaryTreatmentStickyButton');
-            toTopButton.on('click.primaryTreatmentStickyButton', function() {
-                $('html, body').animate({
-                    scrollTop: pt.find('.js-scrolltoexp').offset().top - 80
-                })
-            })
             var scrollBottom = $(window).scrollTop() + $(window).height();
-            var expandable = pt.find('.js-expandable');
             var currentDistance = $(document).scrollTop();
-            var containerHeight = parseInt(expandable.height())+parseInt(pt.offset().top);
-            if (currentDistance > pt.offset().top+20 && scrollBottom < containerHeight) {
-                if (!stickyNav.hasClass('placedtop')) {
-                    stickyNav.addClass('placedtop').removeClass('placedbottom closebutton');
+            pt.each(function() {
+                var currentElement = $(this);
+                var stickyNav = currentElement.find('.js-sticky-expander-nav');
+
+                if (!currentElement.isInViewPort() && stickyNav.hasClass('relative')) {
+                    return;
                 }
-            }
-            else  {
-                if (!stickyNav.hasClass('placedbottom')) {
-                    stickyNav.addClass('placedbottom').removeClass('placedtop closebutton')
+                else {
+                    var expandable = currentElement.find('.js-expandable');
+                    var elementTop = currentElement.offset().top;
+                    var containerHeight = parseInt(expandable.height())+parseInt(elementTop);
+
+                    var toTopButton = stickyNav.find('.scroll-exp');
+                    toTopButton.off('click.primaryTreatmentStickyButton');
+                    toTopButton.on('click.primaryTreatmentStickyButton', function() {
+                        $('html, body').animate({
+                            scrollTop: currentElement.find('.js-scrolltoexp').offset().top - 80
+                        });
+                    });
+
+                    
+
+                    if (currentDistance > elementTop+20 && scrollBottom < containerHeight) {
+                        stickyNav.addClass('sticky').removeClass('relative');
+                    }
+                    else  {
+                        stickyNav.addClass('relative').removeClass('sticky')
+                    }
                 }
-            }
+            });
         }
     }, 50);
-
-    function debounce(func, wait, immediate) {
-        var timeout;
-        return function() {
-            var context = this, args = arguments;
-            var later = function() {
-                timeout = null;
-                if (!immediate) func.apply(context, args);
-            };
-            var callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) func.apply(context, args);
-        };
-    };
 
     var navbar = $('.js-sticky-expander-nav');
     if (navbar.length !== 0) {
@@ -48,7 +44,31 @@ function primaryTreatmentStickyButton() {
     } else {
         $(document).off('primaryTreatmentStickyButton');
     }
-    
 }
+
+$.fn.isInViewPort = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+}
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
 
 window.primaryTreatmentStickyButton = primaryTreatmentStickyButton;
