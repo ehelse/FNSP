aaeac135dda72b183ea4a914cfa0f2b49b8eecf0
AndreasNE89 <hakka_kanon@live.no>
2020-01-15 08:52:34 +0100

    Første commit av prototypekart

---
+++ src/maboxutils.js
@@ -0,0 +1,135 @@
+
+export const ekstruderBygninger = (map) => {
+    map.on('load', function () {
+        // Insert the layer beneath any symbol layer.
+        var layers = map.getStyle().layers;
+
+        var labelLayerId;
+        for (var i = 0; i < layers.length; i++) {
+            if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+                labelLayerId = layers[i].id;
+                break;
+            }
+        }
+
+        map.addLayer(
+            {
+                'id': '3d-buildings',
+                'source': 'composite',
+                'source-layer': 'building',
+                'filter': ['==', 'extrude', 'true'],
+                'type': 'fill-extrusion',
+                'minzoom': 15,
+                'paint': {
+                    'fill-extrusion-color': '#aaa',
+
+                    // use an 'interpolate' expression to add a smooth transition effect to the
+                    // buildings as the user zooms in
+                    'fill-extrusion-height': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        15,
+                        0,
+                        15.05,
+                        ['get', 'height']
+                    ],
+                    'fill-extrusion-base': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        15,
+                        0,
+                        15.05,
+                        ['get', 'min_height']
+                    ],
+                    'fill-extrusion-opacity': 0.3
+                }
+            },
+            labelLayerId
+        );
+    });
+}
+
+export const tegnRute = (map, e, data) => {
+    let end = {
+            type: 'FeatureCollection',
+            features: [{
+                type: 'Feature',
+                geometry: {
+                    type: 'Point',
+                    coordinates: e.geometry.coordinates[0][0]
+                }
+            }]
+        };
+        let route = {
+            'type': 'Feature',
+            "properties": {},
+            'geometry': {
+                'type': 'LineString',
+                'coordinates': data.routes[0].geometry.coordinates
+            }
+        };
+    
+        const start = [[10.995503, 59.933354]]
+        start.push(data.waypoints)
+        
+        map.addLayer({
+            id: 'end',
+            type: 'circle',
+            source: {
+                type: 'geojson',
+                data: end
+            },
+            paint: {
+                'circle-radius': 10,
+                'circle-color': '#f30'
+            }
+        });
+
+        map.addLayer({
+            'id': 'route',
+            'type': 'line',
+            'source': {
+                'type': 'geojson',
+                'data': route
+            },
+            'layout': {
+                'line-join': 'round',
+                'line-cap': 'round'
+            },
+            'paint': {
+                'line-color': '#3887be',
+                'line-width': 6
+            }
+        });
+    }
+
+
+export const addStart = (map, start) => {
+    map.on('load', function () {
+        map.addLayer({
+            id: 'point',
+            type: 'circle',
+            source: {
+                type: 'geojson',
+                data: {
+                    type: 'FeatureCollection',
+                    features: [{
+                        type: 'Feature',
+                        properties: {},
+                        geometry: {
+                            type: 'Point',
+                            coordinates: start
+                        }
+                    }
+                    ]
+                }
+            },
+            paint: {
+                'circle-radius': 10,
+                'circle-color': '#3887be'
+            }
+        });
+    })
+}
---
+++ src/map.jsx
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import { ekstruderBygninger, tegnRute, addStart } from './maboxutils';
+import './App.css';
+import mapboxgl from 'mapbox-gl';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW5kcmVhc2U4OWRldiIsImEiOiJjazUzbmN4bHYwOHkzM2VxdGFkamVicHh4In0.DdGmpKQ-GPNoi-yVBTOXNw';
+const secretToken = 'sk.eyJ1IjoiYW5kcmVhc2U4OWRldiIsImEiOiJjazU2czZwd28wMjZoM3NwODViMzlheDFvIn0.mAGpV2l3t7NSO4pAlqan9w';
+
+export class Map extends Component {
+    state = {
+        lat: 59.931899,
+        lng: 10.995412,
+        zoom: 17,
+        features: null,
+    };
+    componentDidMount() {
+        const map = new mapboxgl.Map({
+            container: this.mapContainer,
+            style: 'mapbox://styles/andrease89dev/ck53v83xd08a31cs5klh6150t',
+            center: [this.state.lng, this.state.lat],
+            zoom: this.state.zoom,
+            pitch: 45,
+            bearing: 20,
+            antialias: true
+        });
+
+        this.setState({ kart: map }, () => this.initialiserKart())
+    }
+    initialiserKart = () => {
+        ekstruderBygninger(this.state.kart)
+        addStart(this.state.kart, [10.995503, 59.933354])
+        this.hentFeatureCollection()
+    }
+    hentStyle = () => {
+        fetch(`https://api.mapbox.com/styles/v1/andrease89dev?access_token=${secretToken}`)
+            .then(data => data.json())
+            .then(formatert => console.log(formatert))
+    }
+    slettGammelData = () => {
+        if (this.state.kart.getLayer('end')) {
+            this.state.kart.removeLayer('end').removeSource('end')
+            this.state.kart.removeLayer('route').removeSource('route')
+        }
+
+    }
+    navigasjon = (start, slutt, feature) => {
+        this.slettGammelData();
+        fetch(
+            `https://api.mapbox.com/directions/v5/mapbox/walking/${start};${slutt}?geometries=geojson&access_token=pk.eyJ1IjoiYW5kcmVhc2U4OWRldiIsImEiOiJjazUzbmN4bHYwOHkzM2VxdGFkamVicHh4In0.DdGmpKQ-GPNoi-yVBTOXNw`)
+            .then((response) => response.json())
+            .then((data) => tegnRute(this.state.kart, feature, data))
+    }
+    hentFeatureCollection = () => {
+        fetch('https://api.mapbox.com/datasets/v1/andrease89dev/ck53uepfu06t42rqhkgg500dk/features?access_token=pk.eyJ1IjoiYW5kcmVhc2U4OWRldiIsImEiOiJjazUzbmN4bHYwOHkzM2VxdGFkamVicHh4In0.DdGmpKQ-GPNoi-yVBTOXNw')
+            .then((response) => response.json())
+            .then((data) => this.setState({ features: data.features }))
+    }
+
+    finnMinLokasjon = (map) => {
+        map.addControl(
+            new mapboxgl.GeolocateControl({
+                positionOptions: {
+                    enableHighAccuracy: true
+                },
+                trackUserLocation: true
+            })
+        );
+    }
+
+    render() {
+        const features = this.state.features
+        return (
+            <div className="App">
+                <div
+                    ref={el => this.mapContainer = el}
+                    className="mapContainer" />
+                <div className='liste-wrapper'>
+                    {features ? features.map((feature, key) => {
+                        return <button
+                            key={key}
+                            className='featurebox'
+                            onClick={() => this.navigasjon([10.995503, 59.933354], feature.geometry.coordinates, feature)}>
+                            {feature.properties.name}
+                        </button>
+                    }) : null}
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default Map;
