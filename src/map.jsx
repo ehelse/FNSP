0bd7d8ee85dc6a974dc3ee1f6daff5582f0999b7
AndreasNE89 <hakka_kanon@live.no>
2020-01-17 09:42:55 +0100

    jobber med touch på mobil

--- src/map.jsx
+++ src/map.jsx
@@ -83,7 +83,7 @@ export class Map extends Component {
                                 if (feature.properties.skalVises !== false) return <option
                                     key={feature.properties.name+key}
                                     className='featurebox-mobil'
-                                    onChange={() => this.navigasjon([10.995503, 59.933354], feature.geometry.coordinates, feature)}>
+                                    onClick={() => this.navigasjon([10.995503, 59.933354], feature.geometry.coordinates, feature)}>
                                     {feature.properties.name}
                                 </option>
                             })}
